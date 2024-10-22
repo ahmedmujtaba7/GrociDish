bb848a1e9b177901842016bf14aa9944f6b35727
Ahmed Mujtaba <ahmed.mujtaba1361@gmail.com>
2024-10-22 23:04:16 +0500

    login/signup

--- Backend_server/src/config/appConfig.ts
+++
@@ -1,5 +0,0 @@
-export const APP_CONFIG = {
-    PORT: process.env.PORT || 3000,
-    ENV: process.env.NODE_ENV || 'development',
-    JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret', // Ensure this is strong and secret in production
-  };
